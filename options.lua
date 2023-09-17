c1921ea9c9e8d87db2243dfd20fa3dd1c202f248
g1fttt <george.goworow2015@gmail.com>
2023-09-17 15:32:21 +0300

    Fix misspelling

--- options.lua
+++ options.lua
@@ -1,7 +1,7 @@
 return function(local_vim)
   local_vim.opt.guifont = "Hack:h12"
 
-  if local_vim.g.neovide then
+  if vim.g.neovide then
     local_vim.g.neovide_hide_mouse_when_typing = true
     local_vim.g.neovide_cursor_vfx_mode = "ripple"
   end
