cb5bce516fccef40573ea322be63ab37c21f44a2
g1fttt <george.goworow2015@gmail.com>
2023-09-18 15:26:36 +0300

    We don`t need that control level at 'options.lua'

--- options.lua
+++ options.lua
@@ -1,12 +1,23 @@
-return function(local_vim)
-  local_vim.opt.guifont = "Hack:h12"
+return {
+  opt = {
+    relativenumber = true,
+    spell = false,
+    signcolumn = "number",
+    guifont = "Hack:h12",
+  },
+  g = {
+    resession_enabled = true, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    neovide_hide_mouse_when_typing = true,
+    neovide_cursor_vfx_mode = "ripple",
+  },
+}
 
-  if vim.g.neovide then
-    local_vim.g.neovide_hide_mouse_when_typing = true
-    local_vim.g.neovide_cursor_vfx_mode = "ripple"
-  end
-
-  local_vim.g.resession_enabled = true
-
-  return local_vim
-end
+-- If you need more control, you can use the function()...end notation
+-- return function(local_vim)
+--   local_vim.opt.relativenumber = true
+--   local_vim.g.mapleader = " "
+--   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+--   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+--
+--   return local_vim
+-- end
