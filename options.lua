2a8e77d297da1d7f248b59ec46b4c0ef2be5f59d
g1fttt <george.goworow2015@gmail.com>
2023-09-19 18:06:52 +0300

    Disable Nerd font's icons if we using non-graphical Neovim client

--- options.lua
+++ options.lua
@@ -1,22 +1,17 @@
-return {
-  opt = {
-    relativenumber = true,
-    spell = false,
-    guifont = "Hack:h12",
-  },
-  g = {
-    resession_enabled = true, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
-    neovide_hide_mouse_when_typing = true,
-    neovide_cursor_vfx_mode = "ripple",
-  },
-}
+return function(local_vim)
+  local_vim.opt.relativenumber = true
+  local_vim.opt.spell = false
 
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
+  local_vim.g.resession_enabled = true
+
+  if vim.g.neovide then
+    local_vim.opt.guifont = "Hack:h12"
+
+    local_vim.g.neovide_hide_mouse_when_typing = true
+    local_vim.g.neovide_cursor_vfx_mode = "ripple"
+  else
+    local_vim.g.icons_enabled = false
+  end
+
+  return local_vim
+end
