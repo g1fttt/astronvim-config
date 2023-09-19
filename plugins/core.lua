e26e92777492d5460d8321bcaf152494aa327b57
g1fttt <george.goworow2015@gmail.com>
2023-09-19 17:33:43 +0300

    Use Nushell as default shell in toggleterm.nvim

--- plugins/core.lua
+++ plugins/core.lua
@@ -21,9 +21,10 @@ return {
     end,
   },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  -- You can disable default plugins as follows:
-  -- { "max397574/better-escape.nvim", enabled = false },
-  --
+  {
+    "akinsho/toggleterm.nvim",
+    opts = function(_, opts) opts.shell = "nu" end
+  }
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
   --   "L3MON4D3/LuaSnip",
