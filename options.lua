a992be93e757f32793c29a12a8209acd061b03c8
g1fttt <george.goworow2015@gmail.com>
2023-09-22 16:22:10 +0300

    Revert options.lua (again)

--- options.lua
+++ options.lua
@@ -1,17 +1,12 @@
-return function(local_vim)
-  local_vim.opt.relativenumber = true
-  local_vim.opt.spell = false
-
-  local_vim.g.resession_enabled = true
-
-  if vim.g.neovide then
-    local_vim.opt.guifont = "Hack Nerd Font:h12"
-
-    local_vim.g.neovide_hide_mouse_when_typing = true
-    local_vim.g.neovide_cursor_vfx_mode = "ripple"
-  else
-    local_vim.g.icons_enabled = false
-  end
-
-  return local_vim
-end
+return {
+  opt = {
+    guifont = "Hack Nerd Font:h12",
+    relativenumber = true,
+    spell = false,
+  },
+  g = {
+    resession_enabled = true,
+    neovide_hide_mouse_when_typing = true,
+    neovide_cursor_vfx_mode = "ripple",
+  }
+}
